90fad83a5c553ea4ab71e8fe0c1f6b9bdced1e04
pprz <xp31@free.fr>
2021-05-04 19:04:29 +0200

    up

--- 1-doc-setup/test.sh
+++ 1-doc-setup/test.sh
@@ -3,8 +3,8 @@
 WORKFOLDER=/home/pprz/Projects/nvidiasandbox
 
 MATERIAL=$WORKFOLDER/Material
-DOWNLOADFOLDER=$MATERIAL/nvidia/Downloads/nvidia/os_sdkm_downloads
-
+OS_SDK=$MATERIAL/nvidia/Downloads/nvidia/os_sdkm_downloads
+CMP_SDK=$MATERIAL/nvidia/Downloads/nvidia/cmp_sdkm_downloads
 APT=$MATERIAL/nvidia/sdkmanager_1.5.0-7774_amd64.deb
 
 L4T=$WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT/Linux_for_Tegra
@@ -12,14 +12,17 @@ L4T=$WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT/Linux_for_Tegra
 #------------------------------------------------------------------------------
 case "$1" in
 
-  "1")
+  "0")
     sudo rm -R ~/.nvsdkm $WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT
     sudo apt-get install $APT
-    sdkmanager --cli downloadonly --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $DOWNLOADFOLDER
+    exit 1;;
+
+  "1")
+    sdkmanager --cli downloadonly --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $OS_SDK
     exit 1;;
 
   "2")
-    sdkmanager --cli install --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $DOWNLOADFOLDER
+    sdkmanager --cli install --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $OS_SDK
     exit 1;;
 
   "3")
@@ -40,24 +43,31 @@ case "$1" in
     screen /dev/ttyUSB0 115200
     exit 1;;
 
+  "6")
+    sdkmanager --cli downloadonly --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --deselect 'Jetson OS' --select 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $CMP_SDK
+    exit 1;;
+
+  "7")
+    sdkmanager --cli install --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --deselect 'Jetson OS' --select 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $CMP_SDK
+    exit 1;;
+
 esac
 
 
-#------------------------------------------------------------------------------
-#dns=8.8.8.8,8.8.4.4
-
-#sudo sysctl net.ipv4.ip_forward=1
-#sudo iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
-
-#------------------------------------------------------------------------------
-#sudo apt-get update
-
-
-#------------------------------------------------------------------------------
-#/boot/extlinux/extlinux.conf
-#      FDT /boot/tegra194-xavier-nx-cti-NGX004-IMX219-2CAM.dtb
-
 : '
+#------------------------------------------------------------------------------
+/etc/NetworkManager/system-connections/...
+
+dns=8.8.8.8,8.8.4.4
+
+------------------------------------------------------------------------------
+sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o wlp59s0 -j MASQUERADE
+
+ssh pprz@192.168.3.2
+sudo apt-get update
+
+------------------------------------------------------------------------------
 sudo fdisk -l |grep GiB
 =>
 Disk /dev/mmcblk1: 29.7 GiB, 31914983424 bytes, 62333952 sectors
@@ -77,4 +87,25 @@ sudo mv local share src /alt
 sudo ln -s /alt/* .
 sudo sync
 sudo reboot
+df
+
+------------------------------------------------------------------------------
+sudo apt install python3-pip
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+
+sudo pip3 install -U pip testresources setuptools==49.6.0
+sudo pip3 install -U numpy==1.19.4 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
+pip3 install torch
+pip3 install torchvision
+#pip3 install torchaudio
+pip3 install serial
+
+------------------------------------------------------------------------------
+Install compagnon-software
+
+------------------------------------------------------------------------------
+/boot/extlinux/extlinux.conf
+      FDT /boot/tegra194-xavier-nx-cti-NGX004-IMX219-2CAM.dtb
+
 '
