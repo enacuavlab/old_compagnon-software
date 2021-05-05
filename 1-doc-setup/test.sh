2c2ebe9265c897421c30548c55a1209d6272db19
pprz <xp31@free.fr>
2021-05-05 19:54:49 +0200

    up

--- 1-doc-setup/test.sh
+++ 1-doc-setup/test.sh
@@ -1,57 +1,80 @@
 #!/bin/bash
 
+VERSION="4.5"
+#VERSION="4.5.1"
 WORKFOLDER=/home/pprz/Projects/nvidiasandbox
-
 MATERIAL=$WORKFOLDER/Material
-OS_SDK=$MATERIAL/nvidia/Downloads/nvidia/os_sdkm_downloads
-CMP_SDK=$MATERIAL/nvidia/Downloads/nvidia/cmp_sdkm_downloads
 APT=$MATERIAL/nvidia/sdkmanager_1.5.0-7774_amd64.deb
-
-L4T=$WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT/Linux_for_Tegra
 
 #------------------------------------------------------------------------------
 case "$1" in
+  nano|xaviernx)
 
-  "0")
-    sudo rm -R ~/.nvsdkm $WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT
-    sudo apt-get install $APT
+    if [ "$1" = "nano" ]; then
+      TARGET="P3448-0000"
+      WORK=$WORKFOLDER/JetPack_4.5_Linux_JETSON_NANO_DEVKIT
+      FLASH=flash.sh jetson-nano-devkit-emmc mmcblk0p1
+    else 
+      TARGET="P3668-0000"
+      WORK=$WORKFOLDER/JetPack_4.5_Linux_JETSON_XAVIER_NX_DEVKIT
+      FLASH=flash.sh cti/xavier-nx/quark-avt mmcblk0p1
+    fi
+
+    OS_SDK=$MATERIAL/nvidia/Downloads/$VERSION/os_sdkm_downloads
+    CMP_SDK=$MATERIAL/nvidia/Downloads/$VERSION/cmp_sdkm_downloads
+    L4T=$WORK/Linux_for_Tegra
+
+    CMD="sdkmanager --logintype devzone --targetos Linux --product Jetson --license accept --version $VERSION --target=$TARGET --targetimagefolder $WORKFOLDER"
+
+    #------------------------------------------------------------------------------
+    case "$2" in
+    
+      "0")
+        echo "sudo rm -R ~/.nvsdkm $WORK"
+        sudo apt-get install $APT
+        exit 1;;
+    
+      "1")
+        $CMD --cli downloadonly --select 'Jetson OS' --deselect 'Jetson SDK Components' --downloadfolder $OS_SDK
+        exit 1;;
+    
+      "2")
+        $CMD --cli install --select 'Jetson OS' --deselect 'Jetson SDK Components' --downloadfolder $OS_SDK
+        exit 1;;
+    
+      "3")
+        tar xvf $MATERIAL/connettech/CTI-L4T-XAVIER-NX-AVT-32.5-V002.tgz -C $L4T
+        cd $L4T/CTI-L4T
+        sudo ./install.sh
+        exit 1;;
+    
+      "4")
+        #lo=`lsusb | grep "NVidia Corp"`
+        #echo $lo
+        cd $L4T
+        sudo $FLASH
+        exit 1;;
+    
+      "5")
+        #ls /dev/ttyUSB0
+        screen /dev/ttyUSB0 115200
+        exit 1;;
+    
+      "6")
+        $CMD --cli downloadonly --deselect 'Jetson OS' --select 'Jetson SDK Components' --downloadfolder $CMP_SDK
+        exit 1;;
+    
+      "7")
+        #ssh pprz@192.168.3.2 ping www.google.com
+        $CMD --cli install --deselect 'Jetson OS' --select 'Jetson SDK Components' --downloadfolder $CMP_SDK
+        exit 1;;
+    
+    esac
     exit 1;;
 
-  "1")
-    sdkmanager --cli downloadonly --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $OS_SDK
+  *)
+    echo "nano/xaviernx 0-7"
     exit 1;;
-
-  "2")
-    sdkmanager --cli install --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --select 'Jetson OS' --deselect 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $OS_SDK
-    exit 1;;
-
-  "3")
-    tar xvf $MATERIAL/connettech/CTI-L4T-XAVIER-NX-AVT-32.5-V002.tgz -C $L4T
-    cd $L4T/CTI-L4T
-    sudo ./install.sh
-    exit 1;;
-
-  "4")
-    #lo=`lsusb | grep "NVidia Corp"`
-    #echo $lo
-    cd $L4T
-    sudo ./flash.sh cti/xavier-nx/quark-avt mmcblk0p1
-    exit 1;;
-
-  "5")
-    #ls /dev/ttyUSB0
-    screen /dev/ttyUSB0 115200
-    exit 1;;
-
-  "6")
-    sdkmanager --cli downloadonly --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --deselect 'Jetson OS' --select 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $CMP_SDK
-    exit 1;;
-
-  "7")
-    #ssh pprz@192.168.3.2 ping www.google.com
-    sdkmanager --cli install --logintype devzone --targetos Linux --product Jetson --version 4.5 --target=P3668-0000 --deselect 'Jetson OS' --select 'Jetson SDK Components' --license accept --targetimagefolder $WORKFOLDER --downloadfolder $CMP_SDK
-    exit 1;;
-
 esac
 
 
