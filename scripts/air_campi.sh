b21664f72865c230f1692a38e6ec818a9c36a00e
xp31 <xp31@free.fr>
2021-09-09 15:01:58 +0200

    up

--- scripts/air_campi.sh
+++ scripts/air_campi.sh
@@ -6,27 +6,41 @@ PIDFILE=/tmp/wfb.pid
 #HEIGHT=480
 WIDTH=1280
 HEIGHT=720
-FPS=10
+FPS=30
 #BITRATE_VIDEO1=400000
 #BITRATE_VIDEO2=400000
 BITRATE_VIDEO1=3000000
 BITRATE_VIDEO2=3000000
 
-rm /tmp/camera*
-/home/pi/Projects/RaspiCV/build/raspicv -t 0 -w $WIDTH -h $HEIGHT -fps $FPS/1 -b $BITRATE_VIDEO1 -g $FPS -vf -hf -cd H264 -n -a ENAC -ae 22 -x /dev/null -r /dev/null -rf gray -o - \
-   | gst-launch-1.0 fdsrc \
+#rm /tmp/camera*
+#/home/pi/Projects/RaspiCV/build/raspicv -t 0 -w $WIDTH -h $HEIGHT -fps $FPS/1 -b $BITRATE_VIDEO1 -g $FPS -vf -hf -cd H264 -n -a ENAC -ae 22 -x /dev/null -r /dev/null -rf gray -o - \
+#   | gst-launch-1.0 fdsrc \
+#    ! h264parse \
+#    ! video/x-h264,stream-format=byte-stream,alignment=au \
+#    ! rtph264pay name=pay0 pt=96 config-interval=1 \
+#    ! udpsink host=127.0.0.1 port=5600 &
+#echo $! >> $PIDFILE
+#sleep 3
+#gst-launch-1.0 shmsrc socket-path=/tmp/camera3 do-timestamp=true \
+#  ! video/x-raw, format=BGR, width=$WIDTH, height=$HEIGHT, framerate=$FPS/1, colorimetry=1:1:5:1  \
+#  ! v4l2h264enc extra-controls="controls,video_bitrate=$BITRATE_VIDEO2" \
+#  ! rtph264pay name=pay0 pt=96 config-interval=1 \
+#  ! udpsink host=127.0.0.1 port=5700 &
+#echo $! >> $PIDFILE
+
+v4l2-ctl --device /dev/video0 \
+  --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat=H264 \
+  --set-parm=$FPS \
+  --set-ctrl video_bitrate=$BITRATE_VIDEO1 \
+  --set-ctrl h264_i_frame_period=10 \
+  --set-ctrl repeat_sequence_header=1 \
+  --stream-mmap=4 --stream-to=- \
+  | gst-launch-1.0 fdsrc \
     ! h264parse \
     ! video/x-h264,stream-format=byte-stream,alignment=au \
-    ! rtph264pay name=pay0 pt=96 config-interval=1 \
-    ! udpsink host=127.0.0.1 port=5600 &
-echo $! >> $PIDFILE
-sleep 3
-gst-launch-1.0 shmsrc socket-path=/tmp/camera3 do-timestamp=true \
-  ! video/x-raw, format=BGR, width=$WIDTH, height=$HEIGHT, framerate=$FPS/1, colorimetry=1:1:5:1  \
-  ! v4l2h264enc extra-controls="controls,video_bitrate=$BITRATE_VIDEO2" \
-  ! rtph264pay name=pay0 pt=96 config-interval=1 \
-  ! udpsink host=127.0.0.1 port=5700 &
-echo $! >> $PIDFILE
+    ! rtph264pay name=pay0 pt=96 config-interval=-1 \
+    ! udpsink host=127.0.0.1 port=5700  > /dev/null 2>&1 &
+  echo $! >> $PIDFILE
 
 #v4l2-ctl --device /dev/video0 \
 #  --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat=H264 \
