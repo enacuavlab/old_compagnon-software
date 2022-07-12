a745cdd73cc17f79f605c823322794a9e584aea4
amtcvx <xp31@free.fr>
2022-07-12 14:57:24 +0200

    Add files via upload

---
+++ scripts/air_camjet_base265.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then exit; fi
+PIDFILE=$1
+
+gst-launch-1.0 nvarguscamerasrc \
+  ! 'video/x-raw(memory:NVMM), format=NV12, width=1280, height=720, framerate=30/1' \
+  ! nvv4l2h265enc insert-sps-pps=true bitrate=2000000 \
+  ! h265parse  \
+  ! rtph265pay name=pay0 pt=96 config-interval=1 \
+  ! udpsink host=127.0.0.1 port=5700 2>&1 &
+echo $! >> $PIDFILE
