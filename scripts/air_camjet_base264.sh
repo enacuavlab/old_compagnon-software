448aeb73a347b18d3ca245a7e3f6c9fae6c9520f
amtcvx <xp31@free.fr>
2022-07-12 14:59:17 +0200

    up

--- scripts/air_camjet_base264.sh
+++ scripts/air_camjet_base264.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Client command to view stream
+#/usr/bin/gst-launch-1.0  udpsrc port=5700 ! application/x-rtp, encoding-name=H264,payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! xvimagesink 
+
 if [ $# != 1 ]; then exit; fi
 PIDFILE=$1
 
