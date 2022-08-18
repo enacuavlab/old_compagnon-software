1790144d2b03754b8f03e11d9850c54cfae9927a
amtcvx <xp31@free.fr>
2022-08-18 11:59:48 +0200

    Update wfb_on.sh

--- scripts/wfb_on.sh
+++ scripts/wfb_on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOME_PRJ=compagnonsoftwarepath
+HOME_PRJ=/home/pprz/Projects/compagnon-software
 
 DEVICES=/proc/net/rtl88XXau
 FILES=/tmp/wfb_*.pid
@@ -8,24 +8,15 @@ FILES=/tmp/wfb_*.pid
 CHANNELS=(36 40 44 48)
 WLS=()
 
+subdirs=`ls  -d $DEVICES/*/ 2> /dev/null`
+for i in $subdirs;do
+  wl=`basename $i`
+  if [[ $(ifconfig | grep -c $wl) == 0 ]]; then WLS+=($wl); fi
+done
 
-if ls $DEVICES 1> /dev/null 2>&1; then
+if [[ ! -z "$WLS" ]]; then
 
   if [[ $(uname -a | grep -c "4.9.*tegra" ) == 1 ]]; then TEGRA=true; else TEGRA=false; fi
-  
-  wls=`ls -d $DEVICES/*/`
-  for i in $wls;do
-    wl=`basename $i`
-    if $TEGRA; then
-      if [[ $(iwconfig $wl | grep -c "Mode:Managed") == 1 ]]; then WLS+=($wl)
-      else
-        if [[ $(ifconfig | grep -c $wl) == 0 ]]; then WLS+=($wl); fi
-      fi
-    else
-       ty=`iw dev $wl info | grep "type" | awk '{print $2}'`
-       if [[ $ty = "managed" ]]; then WLS+=($wl); fi
-    fi
-  done
 
   id=-1
   for wl in ${WLS[@]};do
@@ -43,24 +34,25 @@ if ls $DEVICES 1> /dev/null 2>&1; then
     #id=2
 
     if [ $id -ge 0 ]; then
-	
+
       ph=`iw dev $wl info | grep wiphy | awk '{print "phy"$2}'`
       nb=`rfkill --raw | grep $ph | awk '{print $1}'`
       st=`rfkill --raw | grep $ph | awk '{print $4}'`
       if [ $st == "blocked" ];then `rfkill unblock $nb`;fi
-  
-      if $TEGRA;then
+
+      if $TEGRA; then
+        echo "TEGRA"
         systemctl stop wpa_supplicant.service
         systemctl stop NetworkManager.service
         ifconfig $wl down
         ifconfig $wl up
         iwconfig $wl mode monitor
-        iw reg set DE
+#        iw reg set DE
         iwconfig $wl channel ${CHANNELS[$id]}
       else
         ifconfig $wl down
         iw dev $wl set monitor otherbss
-        iw reg set DE
+#        iw reg set DE
         ifconfig $wl up
         iw dev $wl set channel ${CHANNELS[$id]}
       fi
@@ -75,4 +67,5 @@ if ls $DEVICES 1> /dev/null 2>&1; then
   done
 fi
 
-$HOME_PRJ/scripts/wfb_off.sh
+# Use in case of reload when plugin and plugout
+$HOME_PRJ/scripts/wfb_off.sh 1
